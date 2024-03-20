7768dbfa49f523456af5675622990c5a3062a600
Mtblnt01 <126462765+Mtblnt01@users.noreply.github.com>
2024-03-20 14:57:00 +0100

    0.feladat kesz

---
+++ szoveg_stat/szoveg_stat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szoveg_stat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //0. feladat
+            string forras = "szöveg";
+        }
+    }
+}
