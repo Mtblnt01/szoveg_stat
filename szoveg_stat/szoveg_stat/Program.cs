90113b5e41403f1a84212bff5ff007c323c55ffd
Mtblnt01 <126462765+Mtblnt01@users.noreply.github.com>
2024-03-20 15:14:17 +0100

    2. feladat kesz

--- szoveg_stat/szoveg_stat/Program.cs
+++ szoveg_stat/szoveg_stat/Program.cs
@@ -8,10 +8,27 @@ namespace szoveg_stat
 {
     class Program
     {
+        static string szoveg_visszaf(string s)
+        {
+            string s_visszafele = "";
+            for (int i = s.Length-1; i >= 0; i--)
+            {
+                s_visszafele+=s[i];
+            }
+
+            return s_visszafele;
+        }
+
         static void Main(string[] args)
         {
             //0. feladat
             string forras = "szöveg";
+            //2. feladat
+            string szoveg_visszafele = szoveg_visszaf(forras);
+            Console.WriteLine(szoveg_visszafele);
+
+            Console.WriteLine("Nyomj egy gombot a kilépéshez");
+            Console.ReadKey();
         }
     }
 }
